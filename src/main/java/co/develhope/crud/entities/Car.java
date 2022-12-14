249d152d30a4234a6e803306bb8076dd5729604a
Giovanni <giosal21@gmail.com>
2022-12-14 17:02:02 +0100

    eseCRUD

---
+++ src/main/java/co/develhope/crud/entities/Car.java
@@ -0,0 +1,21 @@
+package co.develhope.crud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String modelName;
+    private String type;
+}
